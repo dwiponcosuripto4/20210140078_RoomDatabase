38d3aed21435d5cd26e49732cf36961c02e173dd
Dwiponco Suripto <d.suripto.ft21@mail.umy.ac.id>
2023-12-06 00:06:30 +0700

    add HalamanHome.kt

--- app/src/main/java/com/example/roomsiswa/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/roomsiswa/navigasi/PengelolaHalaman.kt
@@ -4,8 +4,15 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.roomsiswa.Data.Siswa
 
+@Composable
+fun SiswaApp(navController: NavHostController= rememberNavController()){
+    HostNavigasi(navController = navController)
+}
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SiswaTopAppBar(
@@ -14,4 +21,9 @@ fun SiswaTopAppBar(
     modifier: Modifier = Modifier,
     scrollBehavior: TopAppBarScrollBehavior? = null,
     navigateUp: () -> Unit = {}
-){}
+){
+
+}
+
+@Composable
+fun HostNavigasi(navController: NavHostController, modifier: Modifier = Modifier) {}
