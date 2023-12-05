413f4e31d866e723fecaf91a4492f9b5ea00441b
Dwiponco Suripto <d.suripto.ft21@mail.umy.ac.id>
2023-12-06 00:21:15 +0700

    add SiswaApp in MainActivity untuk menjalankan aplikasi

--- app/src/main/java/com/example/roomsiswa/MainActivity.kt
+++ app/src/main/java/com/example/roomsiswa/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.roomsiswa.Data.Siswa
+import com.example.roomsiswa.navigasi.SiswaApp
 import com.example.roomsiswa.ui.theme.RoomSiswaTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SiswaApp()
                 }
             }
         }
