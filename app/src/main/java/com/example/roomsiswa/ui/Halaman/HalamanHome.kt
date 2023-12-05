e349efb293326dcb3050eb986e7b0576344a8fd2
Dwiponco Suripto <d.suripto.ft21@mail.umy.ac.id>
2023-12-05 23:35:13 +0700

    add fun ListSiswa

--- app/src/main/java/com/example/roomsiswa/ui/Halaman/HalamanHome.kt
+++ app/src/main/java/com/example/roomsiswa/ui/Halaman/HalamanHome.kt
@@ -1,7 +1,10 @@
 package com.example.roomsiswa.ui.Halaman
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -9,14 +12,17 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.roomsiswa.Data.Siswa
 import com.example.roomsiswa.R
@@ -76,8 +82,46 @@ fun HomeScreen(
 fun BodyHome(
     itemSiswa: List<Siswa>,
     modifier: Modifier=Modifier){
-
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ){
+        if (itemSiswa.isEmpty()) {
+            Text(
+                text = stringResource(R.string.deskripsi_no_item),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleLarge
+            )
+        } else {
+            ListSiswa(
+                itemSiswa = itemSiswa,
+                modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.padding_small))
+            )
+        }
+    }
 }
+
+fun ListSiswa(
+    itemSiswa : List<Siswa>,
+    modifier: Modifier=Modifier
+) {
+    LazyColumn(modifier = Modifier){
+        items(items = itemSiswa, key = {it.id}){
+            person ->
+            DataSiswa(
+                siswa = person,
+                modifier = Modifier
+                    .padding(dimensionResource(id = R.dimen.padding_small))
+            )
+        }
+    }
+}
+
+@Composable
+fun DataSiswa (
+    siswa: Siswa,
+    modifier: Modifier = Modifier
+){}
 
 
 
