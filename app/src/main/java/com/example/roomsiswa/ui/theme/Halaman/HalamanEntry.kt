330777c53367704b6070c2cb99faef19dcd75c0c
Dwiponco Suripto <d.suripto.ft21@mail.umy.ac.id>
2023-12-05 21:18:32 +0700

    add object DestinasiEntry

--- app/src/main/java/com/example/roomsiswa/ui/theme/Halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/Halaman/HalamanEntry.kt
@@ -1,2 +1,11 @@
 package com.example.roomsiswa.ui.theme.Halaman
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import com.example.roomsiswa.R
+import com.example.roomsiswa.navigasi.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
+
