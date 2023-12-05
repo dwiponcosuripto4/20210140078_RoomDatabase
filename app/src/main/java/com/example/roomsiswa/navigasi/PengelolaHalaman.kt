b6339c53efa16ac4e34c8017f27b82c712e8398d
Dwiponco Suripto <d.suripto.ft21@mail.umy.ac.id>
2023-12-06 00:17:17 +0700

    add PengelolaHalaman

--- app/src/main/java/com/example/roomsiswa/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/roomsiswa/navigasi/PengelolaHalaman.kt
@@ -1,13 +1,28 @@
 package com.example.roomsiswa.navigasi
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.roomsiswa.Data.Siswa
+import com.example.roomsiswa.R
+import com.example.roomsiswa.ui.Halaman.DestinasiEntry
+import com.example.roomsiswa.ui.Halaman.DestinasiHome
+import com.example.roomsiswa.ui.Halaman.EntrySiswaScreen
+import com.example.roomsiswa.ui.Halaman.HomeScreen
+import java.security.KeyStore.Entry
 
 @Composable
 fun SiswaApp(navController: NavHostController= rememberNavController()){
@@ -22,8 +37,59 @@ fun SiswaTopAppBar(
     scrollBehavior: TopAppBarScrollBehavior? = null,
     navigateUp: () -> Unit = {}
 ){
-
+    CenterAlignedTopAppBar(
+        title = { Text(title) },
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back))
+                }
+            }
+        }
+        )
 }
 
 @Composable
-fun HostNavigasi(navController: NavHostController, modifier: Modifier = Modifier) {}
+fun HostNavigasi(
+    navController: NavHostController, modifier: Modifier = Modifier
+) {
+    NavHost(navController=navController, startDestination = DestinasiHome.route, modifier = Modifier)
+    {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+                )
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.popBackStack()})
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
