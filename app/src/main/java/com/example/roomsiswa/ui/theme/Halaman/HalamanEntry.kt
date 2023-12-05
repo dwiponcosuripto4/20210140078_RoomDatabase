67c069f5d6f417aeeeabe6f1c80a33e1632e612a
Dwiponco Suripto <d.suripto.ft21@mail.umy.ac.id>
2023-12-05 21:22:01 +0700

    add fun EntrySiswaScreen

--- app/src/main/java/com/example/roomsiswa/ui/theme/Halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/Halaman/HalamanEntry.kt
@@ -1,6 +1,7 @@
 package com.example.roomsiswa.ui.theme.Halaman
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
 import com.example.roomsiswa.R
 import com.example.roomsiswa.navigasi.DestinasiNavigasi
 
@@ -9,3 +10,8 @@ object DestinasiEntry: DestinasiNavigasi {
     override val titleRes = R.string.entry_siswa
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntrySiswaScreen(
+
+) {}
